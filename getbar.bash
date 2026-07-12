3e929aeaf7eabee1e78aded25aff1db2cc41ca22
Lenik <pub.lenik@bodz.net>
2026-07-12 19:26:00 +0800

    Update English man page and bash completion for --delayed.
    getbar(1): rename -d/--delay-started to -d/--delayed=TIMEOUT[um][s];
    document -d0 for infinite wait and positive timeout forcing the data
    phase. --window counts from connection start; -p offset is stripped
    pre-data delay in seconds; --count duration origin follows data phase
    when -d is set.
    
    Bash completion: --delayed replaces --delay-started.

--- getbar.bash
+++ getbar.bash
@@ -6,7 +6,7 @@ _getbar()
 	_init_completion || return
 
 	if [[ $cur == -* ]]; then
-        COMPREPLY=($(compgen -W '-b -i -s -w -p -g -f -e -c -d -v -q -h --block-size --interval --size-max --window --polynomial --gnuplot --force --estimate-bps --count --delay-started --verbose --quiet --help --version' -- "$cur"))
+        COMPREPLY=($(compgen -W '-b -i -s -w -p -g -f -e -c -d -v -q -h --block-size --interval --size-max --window --polynomial --gnuplot --force --estimate-bps --count --delayed --verbose --quiet --help --version' -- "$cur"))
 		return
 	fi
 
